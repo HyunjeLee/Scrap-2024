2c3dc8fadf787db85cd4e5954fe08eddf70aba5f
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-04-04 21:24:38 +0900

    feat: 카테고리 화면 구현

--- app/src/main/java/com/scrap/scrap2024/nav/CategoryFragment.kt
+++ app/src/main/java/com/scrap/scrap2024/nav/CategoryFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.scrap.scrap2024.CategoryAdapter
 import com.scrap.scrap2024.databinding.FragmentCategoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+// 테스트 데이터 // 카테고리
+private var categoryList = mutableListOf(
+    mutableListOf("아이템 1", "22"),
+    mutableListOf("아이템 2", "3"),
+    mutableListOf("아이템 3", "44")
+)
 
 /**
  * A simple [Fragment] subclass.
@@ -40,6 +49,9 @@ class CategoryFragment : Fragment() {
         binding = FragmentCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // recyclerview 어댑터 연결
+        binding.recyclerViewCategory.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewCategory.adapter = CategoryAdapter(categoryList)
 
 
         return view
