a4c7abfb7666675954dcc3445f6617414fc5ea26
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-04-05 21:56:03 +0900

    update: 불필요 코드 삭제

--- app/src/main/java/com/scrap/scrap2024/nav/CategoryFragment.kt
+++ app/src/main/java/com/scrap/scrap2024/nav/CategoryFragment.kt
@@ -9,10 +9,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.scrap.scrap2024.CategoryAdapter
 import com.scrap.scrap2024.databinding.FragmentCategoryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 // 테스트 데이터 // 카테고리
 private var categoryList = mutableListOf(
@@ -21,24 +17,12 @@ private var categoryList = mutableListOf(
     mutableListOf("아이템 3", "44")
 )
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var binding: FragmentCategoryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -57,23 +41,4 @@ class CategoryFragment : Fragment() {
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
