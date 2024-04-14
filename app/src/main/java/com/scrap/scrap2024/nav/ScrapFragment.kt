57ba39a7be8b18a591a5af057ccbe729095c7a0e
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-04-14 20:51:41 +0900

    feat: 맨 위로 가기 기능 구현

--- app/src/main/java/com/scrap/scrap2024/nav/ScrapFragment.kt
+++ app/src/main/java/com/scrap/scrap2024/nav/ScrapFragment.kt
@@ -1,15 +1,16 @@
 package com.scrap.scrap2024.nav
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.scrap.scrap2024.adapter.ScrapAdapter
 import com.scrap.scrap2024.data.scrapList
 import com.scrap.scrap2024.databinding.FragmentScrapBinding
-
 
 
 class ScrapFragment : Fragment() {
@@ -30,6 +31,22 @@ class ScrapFragment : Fragment() {
 
         binding.recyclerViewScrap.layoutManager = GridLayoutManager(context, 2)
         binding.recyclerViewScrap.adapter = scrapAdapter
+
+        binding.recyclerViewScrap.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                Log.i("_SCROLL_OFFSET", "offset :${recyclerView.computeVerticalScrollOffset()}")
+                if (recyclerView.computeVerticalScrollOffset() == 0) {
+                    binding.fabUp.hide()
+                } else {
+                    binding.fabUp.show()
+                }
+            }
+        })
+        binding.fabUp.setOnClickListener {
+            binding.recyclerViewScrap.smoothScrollToPosition(0)
+        }
 
         return binding.root
     }
