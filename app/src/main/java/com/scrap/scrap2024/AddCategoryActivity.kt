414d8970074f51eb3f0bac98c9924a46cb3b810d
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-04-16 13:36:37 +0900

    feat: 아무것도 입력하지 않을 시 & 21글자 초과 시 버튼 변경

--- app/src/main/java/com/scrap/scrap2024/AddCategoryActivity.kt
+++ app/src/main/java/com/scrap/scrap2024/AddCategoryActivity.kt
@@ -2,7 +2,11 @@ package com.scrap.scrap2024
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -37,13 +41,38 @@ class AddCategoryActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.i("_TEXT_STATUS", "create : ${binding.editTextAddCategory.text.toString()}")
 
         // 카테고리 추가 취소 시
         binding.buttonCancel.setOnClickListener(clickCancelListener)
         binding.buttonBack.setOnClickListener(clickCancelListener)
-
         // 카테고리 추가 시
         binding.buttonAdd.setOnClickListener(clickAddListener)
 
+        // editText의 상태에 따른 추가하기 버튼 활성화 함수
+        binding.editTextAddCategory.addTextChangedListener(object : TextWatcher {
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateButtonState(s)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun updateButtonState(text: CharSequence?) {
+        // edittext가 비어있거나 21글자 초과 시 버튼 비활성화
+        if (text.isNullOrEmpty() || text.length > 21) {
+            binding.buttonAdd.isEnabled = false
+            binding.buttonAdd.backgroundTintList =
+                ColorStateList.valueOf(getColor(R.color.button_deactivated))
+            binding.buttonAdd.setTextColor(getColor(R.color.black))
+        } else {    // 그 외의 경우 버튼 활성화
+            binding.buttonAdd.isEnabled = true
+            binding.buttonAdd.backgroundTintList =
+                ColorStateList.valueOf(getColor(R.color.main_heavy))
+            binding.buttonAdd.setTextColor(getColor(R.color.white))
+        }
     }
 }
