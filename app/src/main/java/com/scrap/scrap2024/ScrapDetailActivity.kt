68666652a2c6bc888bbf029b0d33dbaa40b96ee0
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-04-30 02:51:24 +0900

    feat: 뒤로가기 버튼 기능 구현

--- app/src/main/java/com/scrap/scrap2024/ScrapDetailActivity.kt
+++ app/src/main/java/com/scrap/scrap2024/ScrapDetailActivity.kt
@@ -15,5 +15,7 @@ class ScrapDetailActivity : AppCompatActivity() {
 
         // 아이콘의 original color 구현 위함 // xml 상에서 적용 불가하므로 코드에서 구현
         binding.bottomNavigationView.itemIconTintList = null
+        // 뒤로가기 버튼 기능 구현
+        binding.buttonBack.setOnClickListener { finish() }
     }
 }
