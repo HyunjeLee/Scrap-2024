aa942b91f3f493a2a3c5d0d5cea542c0e5a843fa
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-05-05 10:30:27 +0900

    update: totalScrap 출력 및 로그아웃,회원탈퇴 버튼 구현
    - 로그아웃, 회원탈퇴 버튼의 경우 클릭 시 해당 액티비티 종료 후 로그인화면으로 돌아갑니다.

--- app/src/main/java/com/scrap/scrap2024/nav/MypageFragment.kt
+++ app/src/main/java/com/scrap/scrap2024/nav/MypageFragment.kt
@@ -5,21 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.scrap.scrap2024.R
+import com.scrap.scrap2024.databinding.FragmentMypageBinding
 
 
 class MypageFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentMypageBinding
+
+    // 테스트 데이터 // TODO: api 연결 시 삭제
+    private val totalScrap: Int = (0..999).random()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // 레이아웃 inflate
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
+
+        // totalScrap 출력
+        binding.textCount1.text = (totalScrap / 100).toString()
+        binding.textCount2.text = ((totalScrap % 100) / 10).toString()
+        binding.textCount3.text = (totalScrap % 10).toString()
+
+        // 로그아웃 시 액티비티 종료
+        binding.buttonLogout.setOnClickListener { requireActivity().finish() }
+        // 회원탈퇴 시 액티비티 종료 // TODO: 추후 api 연결 필요
+        binding.buttonWithdrawal.setOnClickListener { requireActivity().finish() }
+
+        return binding.root
     }
 
 }
