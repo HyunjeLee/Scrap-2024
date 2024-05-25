4ad189e106535121767b0867b3d119c0ce74db65
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-05-26 00:39:12 +0900

    feat: 수정 버튼 클릭 이벤트 구현

--- app/src/main/java/com/scrap/scrap2024/nav/ScrapFragment.kt
+++ app/src/main/java/com/scrap/scrap2024/nav/ScrapFragment.kt
@@ -1,11 +1,13 @@
 package com.scrap.scrap2024.nav
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.scrap.scrap2024.R
@@ -63,7 +65,43 @@ class ScrapFragment : Fragment() {
             }
         }
 
+        // 수정 버튼 클릭 시
+        binding.buttonEdit.setOnClickListener {
+            // 카테고리명 수정 활성화
+            activateEdit()
+
+            binding.buttonEdit.visibility = View.GONE
+            binding.buttonDelete.visibility = View.GONE
+            binding.buttonEditCheck.visibility = View.VISIBLE
+        }
+
+        // 수정 완료 시
+        binding.buttonEditCheck.setOnClickListener {
+            binding.buttonEdit.visibility = View.VISIBLE
+            binding.buttonDelete.visibility = View.VISIBLE
+            binding.buttonEditCheck.visibility = View.GONE
+
+            // 카테고리명 수정 비활성화
+            binding.editTextCategoryTitle.isEnabled = false
+            // 카테고리명 수정
+        }
+
         return binding.root
+    }
+
+    private fun activateEdit() {
+        // editText 활성화
+        binding.editTextCategoryTitle.isEnabled = true
+
+        // 커서 표시
+        binding.editTextCategoryTitle.requestFocus()
+        // 텍스트의 맨 끝으로 커서 위치 변경
+        binding.editTextCategoryTitle.setSelection(binding.editTextCategoryTitle.length())
+
+        // 키보드 표시
+        val imm =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.editTextCategoryTitle, InputMethodManager.SHOW_IMPLICIT)
     }
 
 }
