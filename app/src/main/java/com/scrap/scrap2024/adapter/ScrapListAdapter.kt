aefb66f2e0567a1171a81fd8e3619e3cfc2644ac
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-05-29 00:49:18 +0900

    feat: list adapter 생성

---
+++ app/src/main/java/com/scrap/scrap2024/adapter/ScrapListAdapter.kt
@@ -0,0 +1,20 @@
+package com.scrap.scrap2024.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.scrap.scrap2024.data.Scrap
+
+class ScrapListAdapter(private val scrapList: MutableList<Scrap>) :
+    RecyclerView.Adapter<ScrapGridAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScrapGridAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ScrapGridAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
