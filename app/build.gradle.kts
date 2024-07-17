f64c850c4f48a1d6e209e29c58ad7aa98e95b043
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-07-17 12:44:49 +0900

    docs#72:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "feat/#61-SC-99"
+        versionName = "feat/#72-SR-3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
