0c89e0f9c387e70a0f31c7a4fc21069f9f3c32bc
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-07-21 18:50:23 +0900

    feat#72: 검색-카테고리 설정 화면 생성

---
+++ app/src/main/java/com/scrap/scrap2024/ui/SearchSetCategoryActivity.kt
@@ -0,0 +1,21 @@
+package com.scrap.scrap2024.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.scrap.scrap2024.R
+
+class SearchSetCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_search_set_category)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
