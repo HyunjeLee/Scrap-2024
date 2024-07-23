60772f73a8de8441349b6f87adb7246f10b3d384
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-07-23 22:07:32 +0900

    feat#72: 카테고리 클릭 시 배경색 변경 및 체크 이미지 표시, list에 해당 값 추가

---
+++ app/src/main/java/com/scrap/scrap2024/adapter/CategorySetAdapter.kt
@@ -0,0 +1,75 @@
+package com.scrap.scrap2024.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.scrap.scrap2024.R
+import com.scrap.scrap2024.data.Category
+import com.scrap.scrap2024.databinding.ItemCategoryBinding
+
+class CategorySetAdapter(
+    private val categoryList: MutableList<Category>
+) : RecyclerView.Adapter<CategorySetAdapter.ViewHolder>() {
+
+    val list = mutableSetOf<String>()
+
+    // recyclerview에서 하나의 아이템애 해당되는 viewHolder 정의 // item_category.xml 레이아웃 사용
+    inner class ViewHolder(private val binding: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private var isClicked: Boolean = false
+
+        // 카테고리명 바인딩
+        fun bind(item: Category) {
+            binding.textCategoryTitle.text = item.title
+
+            binding.textCategoryCount.visibility = View.GONE
+            binding.imageCount.visibility = View.GONE
+        }
+
+        // 클릭 토글
+        fun toggleClick(item: Category) {
+            binding.root.setOnClickListener {
+                if (isClicked) {
+                    // 배경색 복귀 및 체크 이미지 숨김
+                    it.setBackgroundResource(R.color.main)
+                    binding.ivCheck.visibility = View.GONE
+                    // list에서 해당 값 제거
+                    list.remove(item.title)
+                    // 상태 변경
+                    isClicked = false
+                } else {
+                    // 배경색 변경 및 체크 이미지 표시
+                    it.setBackgroundResource(R.color.main_light)
+                    binding.ivCheck.visibility = View.VISIBLE
+                    // list에 해당 값 추가
+                    list.add(item.title)
+                    // 상태 변경
+                    isClicked = true
+                }
+
+            }
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategorySetAdapter.ViewHolder {
+        val binding =
+            ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategorySetAdapter.ViewHolder, position: Int) {
+        val item = categoryList[position]
+        holder.bind(item)
+        holder.toggleClick(item)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryList.size
+    }
+}
--- app/src/main/java/com/scrap/scrap2024/ui/SearchSetCategoryActivity.kt
+++ app/src/main/java/com/scrap/scrap2024/ui/SearchSetCategoryActivity.kt
@@ -1,18 +1,51 @@
 package com.scrap.scrap2024.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.scrap.scrap2024.R
+import com.scrap.scrap2024.adapter.CategorySetAdapter
+import com.scrap.scrap2024.data.categoryList
 import com.scrap.scrap2024.databinding.ActivitySearchSetCategoryBinding
 
 class SearchSetCategoryActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivitySearchSetCategoryBinding
+    private val binding by lazy { ActivitySearchSetCategoryBinding.inflate(layoutInflater) }
+    private val categorySetAdapter = CategorySetAdapter(categoryList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySearchSetCategoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 상단 화면 이름 바인딩
         binding.viewTitleWithBack.textTitle.text = getString(R.string.category)
+
+        // recyclerveiw 초기화
+        initRecyclerview()
+
+
+        // 아이템 출력
+        binding.btnComplete.setOnClickListener {
+            Toast.makeText(this, categorySetAdapter.list.sorted().toString(), Toast.LENGTH_SHORT)
+                .show()
+        }
+
+    }
+
+    private fun initRecyclerview() {
+        // recyclerview 어댑터 연결
+        binding.rvSetCategory.layoutManager = LinearLayoutManager(this)
+        binding.rvSetCategory.adapter = categorySetAdapter
+
+        // recyclerview의 아이템 간 분할선 추가
+        binding.rvSetCategory.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                LinearLayoutManager.VERTICAL
+            )
+        )
+
     }
 }
